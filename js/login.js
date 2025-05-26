3de2f4f94ba52cf90641b7507cacc7aecebae242
Clancyyks <164584508+Clancyyks@users.noreply.github.com>
2025-05-26 20:36:26 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,19 +1,16 @@
-const loginForm = document.getElementById("loginForm");
-const mensagem = document.getElementById("mensagem");
-
-loginForm.addEventListener("submit", function (e) {
+document.getElementById("loginForm").addEventListener("submit", function (e) {
   e.preventDefault();
-  const email = document.getElementById("email").value;
-  const senha = document.getElementById("senha").value;
 
+  const email = document.getElementById("emailLogin").value;
+  const senha = document.getElementById("senhaLogin").value;
   const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
-  const usuarioEncontrado = usuarios.find(user => user.email === email && user.senha === senha);
 
-  if (usuarioEncontrado) {
-    localStorage.setItem("usuarioLogado", JSON.stringify(usuarioEncontrado));
-    window.location.href = "perfil.html";
-  } else {
-    mensagem.textContent = "E-mail ou senha inválidos!";
-    mensagem.style.color = "red";
-  }
+  const usuario = usuarios.find(u => u.email === email && u.senha === senha);
+  
+if (usuario.perfil === "Administrador") {
+  window.location.href = "adm.html";
+} else {
+  window.location.href = "perfil.html";
+}
+
 });
